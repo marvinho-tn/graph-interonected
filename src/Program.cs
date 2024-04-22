91cdb0dc14cdd3ff246b9f2751346625b13a8b41
marvinho-tn <163774456+marvinho-tn@users.noreply.github.com>
2024-04-22 19:29:26 +0000

    gitignore\GraphInterconected

---
+++ src/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using GraphProject.Models;
+
+namespace GraphProject
+{
+    public class Program
+    {
+        public Program(params string[] args)
+        {
+            var graphone = new Graph();
+            var graphtwo = new Graph();
+            var graphthree = new Graph();
+            var graphfour = new Graph();
+
+            graphone.Chield = graphtwo;
+            graphone.Left = graphthree;
+            graphone.Parent = graphfour;
+            graphone.Right = graphone;
+
+            var four2 = Graph.Search(graphtwo);
+            var four3 = Graph.Search(graphthree);
+            var four4 = Graph.Search(graphfour);
+
+            Console.WriteLine(JsonSerializer.Serialize(new {
+                four2,
+                four3,
+                four4
+            }));
+        }
+    }
+
+}
