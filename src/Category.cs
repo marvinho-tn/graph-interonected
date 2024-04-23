a38d611dee585a2ee6ced5a779ba21a00e26ed87
marvinho-tn <163774456+marvinho-tn@users.noreply.github.com>
2024-04-23 09:06:10 -0300

    Atualizar o Category.cs

--- src/Category.cs
+++ src/Category.cs
@@ -2,9 +2,11 @@ namespace GraphProject.Models
 {
     public class Category : Graph
     {
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public string? Value { get; set; }
         public string? Description { get; set; }
+        public string? Signification { get; set; }
         public Category? Parent { get; set; }
+        public Category? Chield { get; set; }
     }
 } 
