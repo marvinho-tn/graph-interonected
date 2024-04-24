eb0ed08afc52d47c125cc05fc01b13500711836e
marvinho-tn <marvinthomaz@gmail.com>
2024-04-24 15:03:59 +0000

    24 de abr. de 2024, 12:03

--- src/models/Graph.cs
+++ src/models/Graph.cs
@@ -1,29 +1,18 @@
 namespace Graph.Interconected.Models
 {
-    public class Node<T> where T : class
+    public class Node<T>(T value) where T : class
     {
-        public Node<T>? Right { get; set; }
-        public Node<T>? Left { get; set; }
-        public Node<T>? Top { get; set; }
-        public Node<T>? Bottom { get; set; }
+        public T Value { get; set; } = value;
+        public List<Node<T>> Connecteds { get; set; } = [];
     }
 
     public class Graph<T> where T : class
     {
-        public Node<T>? Root { get; set; }
+        private Node<T> Root { get; set; }
 
-        private void Add(Node<T> item)
-        { 
-            if(Root is null)
-                Root = item;
-            else if(Root.Top is null)
-                Root.Top = item;
-            else if(Root.Right is null)
-                Root.Right = item;
-            else if(Root.Bottom is null)
-                Root.Bottom = item;
-            else if(Root.Left is null)
-                Root.Left = item;
+        public void Add(Node<T> item)
+        {
+            Root.Connecteds.Add(item);
         }
     }
 }
