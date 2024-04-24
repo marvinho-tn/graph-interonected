5de404ab54ef8f204b6c50e9ea8bbabf48d3d36a
marvinho-tn <marvinthomaz@gmail.com>
2024-04-24 14:51:00 +0000

    24 de abr. de 2024, 11:51

--- src/models/Graph.cs
+++ src/models/Graph.cs
@@ -1,24 +1,31 @@
 namespace Graph.Interconected.Models
 {
-    public class RootNode<T> where T : class
-    {
-        public Node<T>? Root { get; set; }
-    }
-
     public class Node<T> where T : class
     {
-        Node<T>? Right { get; set; }
-        Node<T>? Left { get; set; }
-        Node<T>? Top { get; set; }
-        Node<T>? Bottom { get; set; }
+        public Node<T>? Right { get; set; }
+        public Node<T>? Left { get; set; }
+        public Node<T>? Top { get; set; }
+        public Node<T>? Bottom { get; set; }
     }
 
-    public class Graph<T> : RootNode<T> where T : class
+    public class Graph<T> where T : class
     {
+        public Node<T>? Root { get; set; }
+
         private void Add(Node<T> item)
         { 
-            if(Root == null)
+            if(Root is null)
                 Root = item;
+            else if(Root.Top is null)
+                Root.Top = item;
+            else if(Root.Right is null)
+                Root.Right = item;
+            else if(Root.Bottom is null)
+                Root.Bottom = item;
+            else if(Root.Left is null)
+                Root.Left = item;
+
+            
         }
     }
 }
