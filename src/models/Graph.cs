f821856995b5a8d0a462a9e022b491fcaf96d2fd
marvinho-tn <marvinthomaz@gmail.com>
2024-04-25 04:25:34 +0000

    25 de abr. de 2024, 01:25

--- src/models/Graph.cs
+++ src/models/Graph.cs
@@ -3,6 +3,7 @@ namespace Graph.Interconected.Models
     public class Node<T>(T value) where T : class
     {
         public T Value { get; set; } = value;
+        public Connection<T>? Connection { get; set; }
     }
 
     public class Connection<T>(Node<T> aource) where T : class
